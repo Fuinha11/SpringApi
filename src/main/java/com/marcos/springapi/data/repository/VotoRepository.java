f92581ce7e1477aaab8222cb147273662cf9cbc8
Fuinha <marcosdgmarques@gmail.com>
2020-11-20 14:00:53 -0300

    Create Database Repositories.

---
+++ src/main/java/com/marcos/springapi/data/repository/VotoRepository.java
@@ -0,0 +1,9 @@
+package com.marcos.springapi.data.repository;
+
+import com.marcos.springapi.data.domain.Voto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VotoRepository extends JpaRepository<Voto, Long> {
+}
